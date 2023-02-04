5ac5fcd00062dbc67ae06d3872a9bccc6151b918
Jelby <evgeniy.ipatov92@mail.ru>
2023-02-04 14:38:28 +0300

    feat: add base structure

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,66 @@
+<script setup lang="ts">
+import { ref, defineAsyncComponent } from "vue";
+import TheHeader from "@/components/TheHeader.vue";
+import TheFooter from "@/components/TheFooter.vue";
+
+const activeTab = ref<string | null>(null);
+const tabs = [
+  {
+    title: "Конвертер",
+    value: "currencyConverter",
+    component: defineAsyncComponent(
+      () => import("@/components/CurrencyConverter.vue")
+    ),
+  },
+  {
+    title: "Список валют",
+    value: "currencyList",
+    component: defineAsyncComponent(
+      () => import("@/components/CurrencyList.vue")
+    ),
+  },
+];
+</script>
+
 <template>
-  <div>Старт приложения</div>
+  <v-app>
+    <TheHeader></TheHeader>
+
+    <v-main>
+      <v-container>
+        <v-row class="mt-10">
+          <v-col clos="3">
+            <v-tabs v-model="activeTab" direction="vertical" grow>
+              <v-tab
+                v-for="(tab, index) in tabs"
+                :key="index"
+                :value="tab.value"
+              >
+                {{ tab.title }}
+              </v-tab>
+            </v-tabs>
+          </v-col>
+
+          <v-col cols="9">
+            <v-window v-model="activeTab">
+              <v-window-item
+                v-for="(tab, index) in tabs"
+                :key="index"
+                :value="tab.value"
+              >
+                <component :is="tab.component"></component>
+              </v-window-item>
+            </v-window>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <TheFooter></TheFooter>
+  </v-app>
 </template>
 
-<style scoped>
+<style>
 .v-container {
   max-width: 1280px;
 }
